cf686aa9d5bf82e48571f71a1a72073935e65a18
KeerthikaPrem <159062201+KeerthikaPrem@users.noreply.github.com>
2024-07-05 19:33:06 +0530

    Stop Watch

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function (){
+
+  var seconds = 0;
+  var tens = 0;
+  var appendTens = document.getElementById("tens")
+  var appendSeconds = document.getElementById("seconds")
+  var buttonStart = document.getElementById("button-start");
+  var buttonStop = document.getElementById("button-stop");
+  var buttonReset = document.getElementById('button-reset');
+  var Interval ;
+
+  buttonStart.onclick = function(){
+
+    clearInterval(Interval);
+    Interval = setInterval(startTimer,10)
+  }
+
+}
